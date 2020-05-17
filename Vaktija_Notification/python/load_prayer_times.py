b90174c0b2a15a1064c4ca32f69639c982c636ef
Adis <hllvc@icloud.com>
2020-05-17 13:11:48 +0200

    Initial Commit

---
+++ Vaktija_Notification/python/load_prayer_times.py
@@ -0,0 +1,31 @@
+import json,requests
+
+content = requests.get("https://api.vaktija.ba/vaktija/v1/14")
+prayer_times = json.loads(content.content)
+
+with open("/home/hllvc/Documents/Git_Repos/Vaktija_Notification/data/prayer_location", "w+") as the_file:
+    the_file.write(prayer_times["lokacija"])
+    
+with open("/home/hllvc/Documents/Git_Repos/Vaktija_Notification/data/prayer_date", "w+") as the_file:
+    the_file.write(prayer_times["datum"][0])
+
+with open("/home/hllvc/Documents/Git_Repos/Vaktija_Notification/data/prayer_gdate", "w+") as the_file:
+    the_file.write(prayer_times["datum"][1])
+
+with open("/home/hllvc/Documents/Git_Repos/Vaktija_Notification/data/prayer_dawn", "w+") as the_file:
+    the_file.write(prayer_times["vakat"][0])
+
+with open("/home/hllvc/Documents/Git_Repos/Vaktija_Notification/data/prayer_fajr", "w+") as the_file:
+    the_file.write(prayer_times["vakat"][1])
+
+with open("/home/hllvc/Documents/Git_Repos/Vaktija_Notification/data/prayer_zuhr", "w+") as the_file:
+    the_file.write(prayer_times["vakat"][2])
+
+with open("/home/hllvc/Documents/Git_Repos/Vaktija_Notification/data/prayer_asr", "w+") as the_file:
+    the_file.write(prayer_times["vakat"][3])
+
+with open("/home/hllvc/Documents/Git_Repos/Vaktija_Notification/data/prayer_maghrib", "w+") as the_file:
+    the_file.write(prayer_times["vakat"][4])
+
+with open("/home/hllvc/Documents/Git_Repos/Vaktija_Notification/data/prayer_isha", "w+") as the_file:
+    the_file.write(prayer_times["vakat"][5])
